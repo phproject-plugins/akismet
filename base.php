92aa921b6f38c320440d6cb3674bc4d77ecd9964
Alan Hardman <alanaktion@gmail.com>
2025-07-09 22:41:11 +0000

    PHP 8 + PSR-12

--- base.php
+++ base.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package  Akismet
  * @author   Alan Hardman <alan@phpizza.com>
@@ -7,81 +8,85 @@
 
 namespace Plugin\Akismet;
 
-class Base extends \Plugin {
+use Model\Issue;
+use Model\Issue\Comment;
 
-	/**
-	 * Initialize the plugin, adding authentication hooks and routes
-	 */
-	public function _load() {
-		$f3 = \Base::instance();
-        $this->_hook("model/issue.before_save", array($this, "issueBeforeSave"));
-        $this->_hook("model/issue/comment.before_save", array($this, "commentBeforeSave"));
-	}
+class Base extends \Plugin
+{
+    /**
+     * Initialize the plugin, adding authentication hooks and routes
+     */
+    public function _load()
+    {
+        \Base::instance();
+        $this->_hook("model/issue.before_save", $this->issueBeforeSave(...));
+        $this->_hook("model/issue/comment.before_save", $this->commentBeforeSave(...));
+    }
 
-	/**
-	 * Check if plugin is installed
-	 * @return bool
-	 */
-	public function _installed() {
-		return !!\Base::instance()->get("site.plugins.akismet.api_key");
-	}
+    /**
+     * Check if plugin is installed
+     * @return bool
+     */
+    public function _installed()
+    {
+        return (bool) \Base::instance()->get("site.plugins.akismet.api_key");
+    }
 
-	/**
-	 * Generate page for admin panel
-	 */
-	public function _admin() {
-		$f3 = \Base::instance();
-		if ($f3->exists("POST.api_key")) {
-			if($f3->get("POST.api_key")) {
-				$helper = Helper::instance();
-				if ($helper->isKeyValid($f3->get("POST.api_key"))) {
-					\Model\Config::setVal("site.plugins.akismet.api_key", $f3->get("POST.api_key"));
-				} else {
-					$f3->set("error", "Invalid API key.");
-					$f3->set("POST.api_key", null);
-				}
-			} else {
-				\Model\Config::setVal("site.plugins.akismet.api_key", null);
-				$f3->set("success", "Key removed, spam filter disabled.");
-			}
-		}
-		echo \Helper\View::instance()->render("akismet/view/admin.html");
-	}
+    /**
+     * Generate page for admin panel
+     */
+    public function _admin()
+    {
+        $f3 = \Base::instance();
+        if ($f3->exists("POST.api_key")) {
+            if ($f3->get("POST.api_key")) {
+                $helper = Helper::instance();
+                if ($helper->isKeyValid($f3->get("POST.api_key"))) {
+                    \Model\Config::setVal("site.plugins.akismet.api_key", $f3->get("POST.api_key"));
+                } else {
+                    $f3->set("error", "Invalid API key.");
+                    $f3->set("POST.api_key", null);
+                }
+            } else {
+                \Model\Config::setVal("site.plugins.akismet.api_key", null);
+                $f3->set("success", "Key removed, spam filter disabled.");
+            }
+        }
 
-	/**
-	 * Check for spam before saving issues
-	 *
-	 * @param  \Model\Issue $issue
-	 * @return \Model\Issue
-	 */
-	public function issueBeforeSave(\Model\Issue $issue) {
-		if ($issue->changed("description")) {
-			$helper = Helper::instance();
-			if ($helper->isSpam($issue->description, "issue")) {
-				$f3 = \Base::instance();
-				$log = new \Log("akismet.log");
-				$log->write("Issue blocked: " . $f3->get("IP") . " - " . $f3->get("AGENT"));
-				throw new \Exception("Spam detected, not saving.");
-			}
-		}
-		return $issue;
-	}
+        echo \Helper\View::instance()->render("akismet/view/admin.html");
+    }
 
-	/**
-	 * Check for spam before saving issues
-	 *
-	 * @param  \Model\Issue\Comment $comment
-	 * @return \Model\Issue\Comment
-	 */
-	public function commentBeforeSave(\Model\Issue\Comment $comment) {
-		$helper = Helper::instance();
-		if ($helper->isSpam($comment->text, "issue")) {
-			$f3 = \Base::instance();
-			$log = new \Log("akismet.log");
-			$log->write("Comment blocked: " . $f3->get("IP") . " - " . $f3->get("AGENT"));
-			throw new \Exception("Spam detected, not saving.");
-		}
-		return $comment;
-	}
+    /**
+     * Check for spam before saving issues
+     */
+    public function issueBeforeSave(Issue $issue): Issue
+    {
+        if ($issue->changed("description")) {
+            $helper = Helper::instance();
+            if ($helper->isSpam($issue->description, "issue")) {
+                $f3 = \Base::instance();
+                $log = new \Log("akismet.log");
+                $log->write("Issue blocked: " . $f3->get("IP") . " - " . $f3->get("AGENT"));
+                throw new \Exception("Spam detected, not saving.");
+            }
+        }
 
+        return $issue;
+    }
+
+    /**
+     * Check for spam before saving issues
+     */
+    public function commentBeforeSave(Comment $comment): Comment
+    {
+        $helper = Helper::instance();
+        if ($helper->isSpam($comment->text, "issue")) {
+            $f3 = \Base::instance();
+            $log = new \Log("akismet.log");
+            $log->write("Comment blocked: " . $f3->get("IP") . " - " . $f3->get("AGENT"));
+            throw new \Exception("Spam detected, not saving.");
+        }
+
+        return $comment;
+    }
 }
--- helper.php
+++ helper.php
@@ -2,85 +2,73 @@
 
 namespace Plugin\Akismet;
 
-class Helper extends \Prefab {
+class Helper extends \Prefab
+{
+    /**
+     * Check if a string is spam
+     *
+     * @param  string $type    One of: issue, comment
+     */
+    public function isSpam(string $string, string $type = "issue", ?string $ip = null, ?string $agent = null): bool
+    {
+        $f3 = \Base::instance();
 
-	/**
-	 * Check if a string is spam
-	 *
-	 * @param  string $string
-	 * @param  string $type    One of: issue, comment
-	 * @param  string $ip
-	 * @param  string $agent
-	 * @return boolean
-	 */
-	public function isSpam($string, $type = "issue", $ip = null, $agent = null) {
-		$f3 = \Base::instance();
+        // Get API key
+        $key = $f3->get("site.plugins.akismet.api_key");
+        if (!$key) {
+            return false;
+        }
 
-		// Get API key
-		$key = $f3->get("site.plugins.akismet.api_key");
-		if (!$key) {
-			return false;
-		}
+        // Get user data
+        if ($ip === null) {
+            $ip = $f3->get("IP");
+        }
 
-		// Get user data
-		if ($ip === null) {
-			$ip = $f3->get("IP");
-		}
-		if ($agent === null) {
-			$agent = $f3->get("AGENT");
-		}
+        if ($agent === null) {
+            $agent = $f3->get("AGENT");
+        }
 
-		// Build request object
-		$url = $f3->get("site.url");
-		$params = array(
-			"blog" => $url,
-			"comment_content" => $string,
-			"comment_type" => $type,
-			"user_ip" => $ip,
-			"user_agent" => $agent,
-		);
-		$payload = http_build_query($params);
+        // Build request object
+        $url = $f3->get("site.url");
+        $params = [
+            "blog" => $url,
+            "comment_content" => $string,
+            "comment_type" => $type,
+            "user_ip" => $ip,
+            "user_agent" => $agent,
+        ];
+        $payload = http_build_query($params);
 
-		// Run request
-		$ch = curl_init("https://$key.rest.akismet.com/1.1/comment-check");
-		curl_setopt($ch, CURLOPT_USERAGENT, "Phproject | Akismet/3.17");
-		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
+        // Run request
+        $ch = curl_init(sprintf('https://%s.rest.akismet.com/1.1/comment-check', $key));
+        curl_setopt($ch, CURLOPT_USERAGENT, "Phproject | Akismet/3.17");
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        return $result == "true";
+    }
 
-		if ($result == "true") {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Check if an Akismet key is valid
+     */
+    public function isKeyValid(string $key): key
+    {
+        // Build request object
+        $url = \Base::instance()->get("site.url");
+        $params = [
+            "key" => $key,
+            "blog" => $url,
+        ];
+        $payload = http_build_query($params);
 
-	/**
-	 * Check if an Akismet key is valid
-	 * @param  string  $key
-	 * @return boolean
-	 */
-	public function isKeyValid($key) {
-		// Build request object
-		$url = \Base::instance()->get("site.url");
-		$params = array(
-			"key" => $key,
-			"blog" => $url,
-		);
-		$payload = http_build_query($params);
-
-		// Run request
-		$ch = curl_init("https://rest.akismet.com/1.1/verify-key");
-		curl_setopt($ch, CURLOPT_USERAGENT, "Phproject | Akismet/3.17");
-		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-
-		if ($result == "valid") {
-			return true;
-		}
-		return false;
-	}
-
+        // Run request
+        $ch = curl_init("https://rest.akismet.com/1.1/verify-key");
+        curl_setopt($ch, CURLOPT_USERAGENT, "Phproject | Akismet/3.17");
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        return $result == "valid";
+    }
 }
